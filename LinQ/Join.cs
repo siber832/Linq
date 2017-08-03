37fe380ef194ea3a0983b7f1f2355fb6c3da51ce
daniel <rodriguezbelmonte@gmail.com>
2017-08-03 18:18:05 +0200

    aqui esta todoerewerreer

---
+++ LinQ/Join.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinQ {
+    public class Join {
+        public static void Execute (List<int> list1, List<int> list2) {
+            Console.WriteLine("Con join.");
+            var join = from number1 in list1
+                join number2 in list2 on number1 equals number2
+                select number1;
+            foreach (var number in join) {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
