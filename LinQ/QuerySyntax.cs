ed669a9fd9690e3cb6bb51790eca96f61a3b6885
daniel <rodriguezbelmonte@gmail.com>
2017-08-03 20:52:22 +0200

    Cambio al correcto

--- LinQ/QuerySyntax.cs
+++ LinQ/QuerySyntax.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace LinQ {
     public class QuerySyntax {
-        public static void Execute (List<int> list1, List<int> list2) {
+        public static void Execute(List<int> list1, List<int> list2) {
             Console.WriteLine("Con query sintax.");
             var query = from number in list1
                 where list2.Contains(number)
